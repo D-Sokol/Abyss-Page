6656fc11c85c5c4d456e475a0920f118a66628b4
Dmitrii Sokol <sokol.dmitry.f@gmail.com>
2022-04-03 18:35:41 +0300

    Fix: create application unconditionally.

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import Config, create_app
 
+config = Config()
+app = create_app(config)
+
 if __name__ == "__main__":
-    config = Config()
-    app = create_app(config)
     app.run(config.HOST, config.PORT, debug=config.DEBUG)
