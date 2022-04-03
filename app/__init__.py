41bd4834c8c13353a5301ac131862e168e0d65b4
Dmitrii Sokol <sokol.dmitry.f@gmail.com>
2022-04-03 16:45:58 +0300

    Add database support.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from flask import Flask
 
 from .routes import bp
+from .models import db
 
 load_dotenv()
 
@@ -11,6 +12,9 @@ class Config:
     HOST = os.getenv("HOST", "localhost")
     PORT = os.getenv("PORT", "5000")
 
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", f"sqlite:///app.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     DEBUG = True
 
 
@@ -18,4 +22,6 @@ def create_app(config: Config) -> Flask:
     app = Flask(__name__)
     app.config.from_object(config)
     app.register_blueprint(bp)
+    db.init_app(app)
+    db.create_all(app=app)
     return app
